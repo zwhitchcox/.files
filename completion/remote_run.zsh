c25d7807607cbcb56400c502817270b241b8854f
Zane Hitchcox <zwhitchcox@gmail.com>
2022-04-24 19:08:16 -0400

    autocomplete remote run

---
+++ completion/remote_run.zsh
@@ -0,0 +1,16 @@
+#/usr/bin/env zsh
+
+_completion_hosts() {
+  hosts=($(sed -e 's/#.*//' -e 's/[[:blank:]]*$//' -e '/^$/d' /etc/hosts | awk '{print $2}' | egrep -v '(local|broadcast)host|kubernetes'))
+  for host in ${hosts[@]}; do
+    compadd $host remote_run
+  done
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+  for prog in `ls $DIR`; do
+    if [ -x $prog ] && ! [ -d $prog ]; then
+      compadd $prog remote_run
+    fi
+  done
+}
+
+compdef _completion_hosts remote_run
