5d7cc134d0ebc0578944e75ef7d4886e28bd7c3b
Zane Hitchcox <Zane>
2022-08-24 14:17:36 -0400

    init

---
+++ _init/arch.sh
@@ -0,0 +1,26 @@
+sudo echo -n '' # acquire sudo permissions early
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+source $SCRIPTPATH/../../env
+source $SCRIPTPATH/common.sh
+
+arch_base() {
+  pacman -Syyu --noconfirm --needed \
+    base-devel \
+    git \
+    jq
+}
+
+SRCDIR=$HOME/src
+BINDIR=$HOME/bin
+
+mkdir -p $SRCDIR
+whichq git || arch_base
+test -f $HOME/.gitconfig || config_git
+whichq snap || install_snap
+whichq gh || install_gh
+gh auth status &>/dev/null || gh_login
+test -f ~/.ssh/id_rsa || keygen
+gh_config
+gh_add_host_keys
+test -d $HOME/bin || init_bin
+init_dotfiles
---
+++ _init/common.sh
@@ -0,0 +1,86 @@
+config_git() {
+  local gc="git config --global user"
+  test -n $gc.email || $gc.email $GIT_EMAIL
+  test -n $gc.name || $gc.email $GIT_NAME
+}
+
+install_snap() {
+  git clone https://aur.archlinux.org/snapd.git $SRCDIR
+  sudo systemctl enable --now snapd.socket
+  sudo ln -s /var/lib/snapd/snap /snap
+}
+
+install_gh() {
+  sudo snap install gh
+}
+
+gh_login() {
+  echo $_GH_TOKEN | gh auth login \
+    -h github.com \
+    -p ssh \
+    --with-token 
+}
+
+keygen() {
+  ssh-keygen -C $GIT_EMAIL -t rsa -b 4096 -f $HOME/.ssh/id_rsa -P ''
+}
+
+
+# get list of keys
+gh_list_keys() {
+  curl \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: token $_GH_TOKEN" \
+    https://api.github.com/user/keys
+}
+
+# get key by title
+gh_key_by_title() {
+  list_keys | jq -r '.[] | "\(.id) \(.title)"'
+}
+
+# delete ssh key
+gh_key_del() {
+  local KEY_ID=$1
+  curl \
+    -X DELETE \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: token $GH_TOKEN" \
+    https://api.github.com/user/keys/$KEY_ID
+}
+
+gh_config() {
+  output=$(cat $HOME/.ssh/id_rsa.pub | gh ssh-key add  -t $(cat /etc/hostname) 2>&1)
+  test $? -eq 0 && return
+  if ! echo $output | grep -q "key is already in use"; then
+    echo -e "could not add key\n$output" > /dev/stderr
+    exit 1
+  fi
+}
+
+# add github host keys to known hosts
+gh_add_host_keys() {
+  local key_file=$HOME/.ssh/known_hosts
+  local keys=$(ssh-keyscan -H github.com 2>/dev/null)
+  (echo $keys ; cat $key_file) | sort | uniq -u > $key_file
+}
+
+init_bin() {
+  pushd $HOME
+  git clone git@github.com:$GH_USERNAME/bin.git $BINDIR || exit 1
+  cd $BINDIR
+  popd
+}
+
+init_dotfiles() {
+  src_rc="source $BINDIR/dotfiles/arch.rc"
+  grep -q $HOME/$(rcfile) "$src_rc" || echo $src_rc >> $HOME/$(rcfile)
+}
+
+whichq() {
+  which $@ > /dev/null
+}
+
+rcfile() {
+  echo -e ".$(basename $(echo -e $SHELL))rc"
+}
