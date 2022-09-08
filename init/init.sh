4247bd0dd5abc3e86119ded5929c31a7fc70a040
Zane Hitchcox <zwhitchcox@gmail.com>
2022-09-08 12:13:46 -0400

    Update init.sh

--- init/init.sh
+++ init/init.sh
@@ -19,8 +19,5 @@ command -v zsh | sudo tee -a /etc/shells
 # use zsh as default shell
 sudo chsh -s $(which zsh) $USER
 
-# bundle zsh plugins 
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
-
 # install neovim plugins
 nvim --headless +PlugInstall +qall
