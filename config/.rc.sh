5c1441eb95e9580a5c3d58a92eaa314e372c40fe
Zane Hitchcox <zwhitchcox@gmail.com>
2023-02-26 15:05:29 -0500

    cargo and nvm

--- config/.rc.sh
+++ config/.rc.sh
@@ -29,3 +29,11 @@ fi
 # Text processing aliases
 alias lower="tr '[:upper:]' '[:lower:]'"
 alias upper="tr '[:lower:]' '[:upper:]'"
+
+if [ -d "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
+source "$HOME/.cargo/env"
